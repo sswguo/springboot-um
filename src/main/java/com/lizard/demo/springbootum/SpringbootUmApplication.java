f0b9724d5fbe29b5f4b5f78176e27c359ec31b78
Wenjie Guo <wguo@redhat.com>
2019-08-05 15:42:58 +0800

    boot app init

---
+++ src/main/java/com/lizard/demo/springbootum/SpringbootUmApplication.java
@@ -0,0 +1,13 @@
+package com.lizard.demo.springbootum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUmApplication.class, args);
+	}
+
+}
