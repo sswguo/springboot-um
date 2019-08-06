23813e770f8a45e361f3db488566ef45c263a446
Wenjie Guo <wguo@redhat.com>
2019-08-06 16:15:08 +0800

    resttemplate test

--- users/src/main/java/com/lizard/demo/springbootum/controllers/UserController.java
+++ users/src/main/java/com/lizard/demo/springbootum/controllers/UserController.java
@@ -3,13 +3,16 @@ package com.lizard.demo.springbootum.controllers;
 import com.lizard.demo.springbootum.model.User;
 import com.lizard.demo.springbootum.operation.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -33,6 +36,15 @@ public class UserController
         u1.setName( username );
         u1.setEmail( email );
         service.addUser(u1);
+
+        RestTemplate restTemplate = new RestTemplate(  );
+
+        MultiValueMap<String, String> mvm = new LinkedMultiValueMap<String, String>();
+        mvm.add("username", u1.getName());
+
+        ResponseEntity<String>
+                        result = restTemplate.postForEntity( "http://notification:8080/notification/mock", mvm, String.class);
+
         return "successfully.";
 
     }
